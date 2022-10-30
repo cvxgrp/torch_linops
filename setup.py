f71d77bbb48ced8f0eb74387a3a03e56d4962308
Parth Nobel <parthnobel@berkeley.edu>
2022-10-30 05:30:27 -0700

    Fixes syntax and bumps version number.

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ with open("README.md", "r") as fh:
 
 setup(
     name="torch-linops",
-    version="0.0.1",
+    version="0.1.0",
     packages=["linops"],
     license="GPLv3",
     description="A library to define abstract linear operators, and associated algebra and matrix-free algorithms, that works with pyTorch Tensors.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     setup_requires=["setuptools>=18.0"],
-    version='0.1.0'
     install_requires=[
         "torch",
         "scipy"
